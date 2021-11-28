f46a64ecd393071df706ab6dd3aa39e1d9f6f82f
dernett <aburnett@tutamail.com>
2021-11-27 17:42:32 -1000

    Draw chessboard squares

--- main.cpp
+++ main.cpp
@@ -1,62 +1,56 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include <raylib.h>
 
+//------------------------------------------------------------------------------------
+// Global Variables Declaration
+//------------------------------------------------------------------------------------
+const int screenWidth = 768;
+const int screenHeight = 768;
+
+const int squareWidth = screenWidth / 8;
+const int squareHeight = screenHeight / 8;
+
+//------------------------------------------------------------------------------------
+// Module Functions Declaration (local)
+//------------------------------------------------------------------------------------
+void DrawGame(); // Draw game (one frame)
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
 int main()
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    int screenWidth = 800;
-    int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Chiaro");
 
     SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+        DrawGame();
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
+}
 
-    return 0;
+// Draw game (one frame)
+void DrawGame()
+{
+    BeginDrawing();
+    {
+        ClearBackground(RAYWHITE);
+
+        // Draw chessboard squares
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if ((i & 1) ^ (j & 1))
+                    DrawRectangle(j * squareWidth, i * squareHeight,
+                                  squareWidth, squareHeight, BROWN);
+                else
+                    DrawRectangle(j * squareWidth, i * squareHeight,
+                                  squareWidth, squareHeight, BEIGE);
+            }
+        }
+    }
+    EndDrawing();
 }
